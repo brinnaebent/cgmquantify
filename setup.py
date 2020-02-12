60ac5284d78b6fa8dea0b06814e52396754e9f03
brinnaebent <brinnae.bent@duke.edu>
2020-02-12 13:51:08 -0500

    Updated ReadMe to be description for PyPI

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+
 setup(name='cgmquantify',
       version='0.1',
-      description='Quantifying glucose and glucose variability from CGM devices',
+      description='Quantifying glucose and glucose variability from CGM devices'
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/brinnaebent/cgmquantify',
       author='Brinnae Bent',
       author_email='bmbent@ncsu.edu',
